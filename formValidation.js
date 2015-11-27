8fabb022c844f147b96dd08dcb64ce5cb66a0ccd
Kimberly Soares <kimberly.soares02@gmail.com>
2015-11-27 18:36:50 -0500

    Javascript file to validate form entries
    When the Submit button the form is clicked, a JavaScript validation function must be run. This function should ensure that
    1. None of the fields listed above is empty.
    2. The email address contains at least one letter to the left of the ‘@’ symbol, at least one character immediately after the ‘@’ symbol, then a period, and then at least one character after the period. The following regular expression may prove useful as you write this function: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.
    3. Years of service is a number between 0 and 50.
    4. The values of Password and Password confirmation match.
    If an error is detected in a field during validation, that field should be colored in red.

---
+++ formValidation.js
@@ -0,0 +1,72 @@
+// Javascript Form Validation File 
+// ID:620076399
+
+function validate()
+{
+	// Get form 
+	var form = document.forms["MPform"];
+
+	if (form.firstName.value == "")
+	{
+		// Error detected, colour field in red
+		form.firstName.style.backgroundColor = "red";
+
+	}
+	if (form.lastName.value == "")
+	{
+		form.lastName.style.backgroundColor = "red";
+
+	}
+	if (form.email.value == "")
+	{
+		form.email.style.backgroundColor = "red";
+
+	}
+	else
+	{
+		// Check the format of the email entered
+		var regEx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+		if (form.email.value.search(regEx) == -1)
+		{
+			form.email.style.backgroundColor = "red";
+		}
+	}
+	if (form.serviceYears.value == "")
+	{
+	
+		form.serviceYears.style.backgroundColor = "red";
+	}
+	else
+	{
+		// Check if the service years entered is between 0 and 50
+		if (!(form.serviceYears.value > 0 && form.serviceYears.value < 50))
+		{
+			form.serviceYears.style.backgroundColor = "red";
+		}
+	}
+
+	if (form.constit.value == "")
+	{
+		form.constit.style.backgroundColor = "red";
+	}
+	
+	if(form.password.value == "" || form.password2.value == "")
+	{
+		
+    	form.password.style.backgroundColor = "red";
+    	form.password2.style.backgroundColor = "red";
+	}
+	else
+	{
+		// Check if the values of passwords entered match
+  		if(form.password.value != form.password2.value)
+  		{
+    		form.password.style.backgroundColor = "red";
+    		form.password2.style.backgroundColor = "red";
+    	}
+	}
+		  
+}
+
+
+
